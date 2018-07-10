bacdede56c323297fc4f4628fc49566fa1d5fce3
Aaron Jack <aaronjackcreative@gmail.com>
2018-07-10 13:28:30 +0100

    commented js to fix form

--- js/main.js
+++ js/main.js
@@ -283,127 +283,127 @@ $(document).ready(function () {
      Contact Form 
      ============================================================ */
 
-    $('.tz_submit').on('click', function (event) {
-        event.preventDefault();
-        var name_attr = [];
-        var values = [];
-        var tz_process = "";
-        if($(this).closest("section").attr('id') !== undefined)
-        {
-            var section_id = $(this).closest("section").attr('id');
-        }else{
-            var section_id = $(this).closest("footer").attr('id');
-        }
-        var submit_loader = '<div class="loading text-deep-green display-inline-block margin-five no-margin-tb no-margin-right" id="loading">Loading...</div>';
-        $('#' + section_id).find('form').find('button').after(submit_loader);
-        $('#' + section_id).find('form input, form select,form textarea').each(
-                function (index) {
-                    
-                    if ($(this).is('[data-email="required"]')) {
-                        var required_val = $(this).val();
-                        if (required_val != '') {
-                            name_attr.push($(this).attr('name'));
-                            values.push($(this).val());
-                            tz_process = true;
-                        } else {
-                            $('#loading').remove();
-                            $(this).addClass('tz_input_error');
-                            tz_process = false;
-                        }
-                    }
-
-                    if (!$(this).is('[data-email="required"]')) {
-                        name_attr.push($(this).attr('name'));
-                        values.push($(this).val());
-                    }
-
-                });
-        
-        var captcha_length = $('.g-recaptcha').length;
-        if (captcha_length >= 1) {
-            var response = grecaptcha.getResponse();
-            //recaptcha failed validation
-            if (response.length == 0) {
-                $('#loading').remove();
-                $('#google-recaptcha-error').remove();
-                $('#' + section_id).find('.g-recaptcha').after('<span class="google-recaptcha-error" id="google-recaptcha-error">Invalid recaptcha</span>');
-                tz_process = false;
-            } else {
-                $('#google-recaptcha-error').remove();
-                $('#recaptcha-error').hide();
-                tz_process = true;
-            }
-        }
-        if (tz_process) 
-        {
-            localStorage.setItem('tz_section',section_id);
-            $.post("tz_mail/contact.php", {
-                data: { input_name: name_attr,values:values,section_id:section_id},
-                type: "POST",
-            }, function (data) {
-                $('#loading').remove();
-                var tz_form_output = '';
-                if(data) 
-                {
-                    if(data.type == "tz_message") 
-                    {
-                       $('#error').remove(); 
-                       $('#success').remove();
-                       $('#google-recaptcha-error').remove(); 
-                       var tz_form_output = '<div id="success" class="no-margin-lr alt-font">'+data.text+'</div>';
-                    }else if (data.type == "tz_error") {
-                        $('#success').remove();
-                        $('#error').remove(); 
-                        var tz_form_output = '<div id="error" class="no-margin-lr alt-font">'+data.text+'</div>';
-                    }else{
-                        var tz_form_output = '';
-                    } 
-                }
-
-                if(tz_form_output != '')
-                {
-                    var section_id = localStorage.getItem('tz_section');
-                    $('#'+section_id).find('form').before(tz_form_output);
-                }
-                $('#' + section_id).find('form input,form textarea').each(function (index) {
-                    $(this).val('');
-                    $(this).removeClass('tz_input_error');
-                });
-
-                setTimeout(function(){
-                    $('#success').fadeOut();
-                    $('#success').remove();
-                    $('#error').fadeOut();
-                    $('#error').remove();
-                    $(this).submit();
-                 },5000);
-                localStorage.removeItem('tz_section');
-            }, 'json');
-        }
-        
-        $('#' + section_id).find('form input,form textarea').each(function (index) {
-            $(this).keypress(function () {
-                $(this).removeClass('tz_input_error');
-            });
-        });
-
-        $('#' + section_id).find('form input,form textarea').each(function (index) {
-            if ($(this).is(":focus")) {
-                $(this).removeClass('tz_input_error');
-            }
-        });
-
-        $('#' + section_id).find('form select').each(function (index) {
-            $(this).on("change", function () {
-                var val = this.value;
-                if (val == ''){
-                    $(this).removeClass('tz_input_error');
-                }
-            });
-        });
-    });
-    
-});
+//    $('.tz_submit').on('click', function (event) {
+//        event.preventDefault();
+//        var name_attr = [];
+//        var values = [];
+//        var tz_process = "";
+//        if($(this).closest("section").attr('id') !== undefined)
+//        {
+//            var section_id = $(this).closest("section").attr('id');
+//        }else{
+//            var section_id = $(this).closest("footer").attr('id');
+//        }
+//        var submit_loader = '<div class="loading text-deep-green display-inline-block margin-five no-margin-tb no-margin-right" id="loading">Loading...</div>';
+//        $('#' + section_id).find('form').find('button').after(submit_loader);
+//        $('#' + section_id).find('form input, form select,form textarea').each(
+//                function (index) {
+//                    
+//                    if ($(this).is('[data-email="required"]')) {
+//                        var required_val = $(this).val();
+//                        if (required_val != '') {
+//                            name_attr.push($(this).attr('name'));
+//                            values.push($(this).val());
+//                            tz_process = true;
+//                        } else {
+//                            $('#loading').remove();
+//                            $(this).addClass('tz_input_error');
+//                            tz_process = false;
+//                        }
+//                    }
+//
+//                    if (!$(this).is('[data-email="required"]')) {
+//                        name_attr.push($(this).attr('name'));
+//                        values.push($(this).val());
+//                    }
+//
+//                });
+//        
+//        var captcha_length = $('.g-recaptcha').length;
+//        if (captcha_length >= 1) {
+//            var response = grecaptcha.getResponse();
+//            //recaptcha failed validation
+//            if (response.length == 0) {
+//                $('#loading').remove();
+//                $('#google-recaptcha-error').remove();
+//                $('#' + section_id).find('.g-recaptcha').after('<span class="google-recaptcha-error" id="google-recaptcha-error">Invalid recaptcha</span>');
+//                tz_process = false;
+//            } else {
+//                $('#google-recaptcha-error').remove();
+//                $('#recaptcha-error').hide();
+//                tz_process = true;
+//            }
+//        }
+//        if (tz_process) 
+//        {
+//            localStorage.setItem('tz_section',section_id);
+//            $.post("tz_mail/contact.php", {
+//                data: { input_name: name_attr,values:values,section_id:section_id},
+//                type: "POST",
+//            }, function (data) {
+//                $('#loading').remove();
+//                var tz_form_output = '';
+//                if(data) 
+//                {
+//                    if(data.type == "tz_message") 
+//                    {
+//                       $('#error').remove(); 
+//                       $('#success').remove();
+//                       $('#google-recaptcha-error').remove(); 
+//                       var tz_form_output = '<div id="success" class="no-margin-lr alt-font">'+data.text+'</div>';
+//                    }else if (data.type == "tz_error") {
+//                        $('#success').remove();
+//                        $('#error').remove(); 
+//                        var tz_form_output = '<div id="error" class="no-margin-lr alt-font">'+data.text+'</div>';
+//                    }else{
+//                        var tz_form_output = '';
+//                    } 
+//                }
+//
+//                if(tz_form_output != '')
+//                {
+//                    var section_id = localStorage.getItem('tz_section');
+//                    $('#'+section_id).find('form').before(tz_form_output);
+//                }
+//                $('#' + section_id).find('form input,form textarea').each(function (index) {
+//                    $(this).val('');
+//                    $(this).removeClass('tz_input_error');
+//                });
+//
+//                setTimeout(function(){
+//                    $('#success').fadeOut();
+//                    $('#success').remove();
+//                    $('#error').fadeOut();
+//                    $('#error').remove();
+//                    $(this).submit();
+//                 },5000);
+//                localStorage.removeItem('tz_section');
+//            }, 'json');
+//        }
+//        
+//        $('#' + section_id).find('form input,form textarea').each(function (index) {
+//            $(this).keypress(function () {
+//                $(this).removeClass('tz_input_error');
+//            });
+//        });
+//
+//        $('#' + section_id).find('form input,form textarea').each(function (index) {
+//            if ($(this).is(":focus")) {
+//                $(this).removeClass('tz_input_error');
+//            }
+//        });
+//
+//        $('#' + section_id).find('form select').each(function (index) {
+//            $(this).on("change", function () {
+//                var val = this.value;
+//                if (val == ''){
+//                    $(this).removeClass('tz_input_error');
+//                }
+//            });
+//        });
+//    });
+//    
+//});
 
 /* ===================================
  shrink navigation
